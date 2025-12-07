adce0864a96d0e935a33d52ad2726cd01db96490
cosmic71a <nguyenminh777804@gmail.com>
2025-12-07 19:18:03 +0700

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
-import express from 'express';
-require("dotenv").config()
-import cors from 'cors'
-import initRoutes from './src/routes';
-import connectDB from './src/config/connectDB';
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+//import initRoutes from "./routes";
+import connectDB from "./config/connectDB.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
