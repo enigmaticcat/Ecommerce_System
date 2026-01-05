9cae1b4239b46acb0f08c42c1cc39dbaca1835e3
ĐỖ NHẬT MINH <donhatminh26102004@gmail.com>
2026-01-05 15:47:56 +0700

    fix confix thêm clouddinary

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,17 @@ const SocketServer = require('./socket/socketServer');
 const initRoutes = require('./routes/index');
 const passport = require('./config/passport');
 const dotenv = require('dotenv'); 
+const cors = require('cors');
 
 dotenv.config(); 
 
 const app = express();
 const server = http.createServer(app);
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true,               
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +27,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Ecommerce_System');
 
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+        console.log(` MongoDB Connected: ${conn.connection.host}`);
 
 
         require('./models/user');
